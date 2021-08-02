5c08664e5ca599157bb6e9ad846d84be36fcba6a
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-08-02 11:15:21 -0400

    random instances of celebrities implemented - MainActivity.java

--- app/src/main/java/com/example/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/guessthecelebrity/MainActivity.java
@@ -10,14 +10,21 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    // variables
+    private ArrayList<String> arrayListURLs, arrayListNames;
+    private int randomCelebrity;
 
     public class PageSourceTask extends AsyncTask<String, Void, String> {
 
@@ -61,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        arrayListURLs = new ArrayList<>();
+        arrayListNames = new ArrayList<>();
+
         PageSourceTask pageSourceTask = new PageSourceTask();
         String result;
 
@@ -74,10 +84,26 @@ public class MainActivity extends AppCompatActivity {
 
             Pattern pattern = Pattern.compile("src=\"(.*?)\"");
             Matcher matcher = pattern.matcher(splitResult[0]);
+            int counter = 0;
 
             while (matcher.find()) {
-                Log.i("URL Content", "" + matcher.group(1));
+
+                counter++;
+
+                if (counter > 5) {
+                    arrayListURLs.add(matcher.group(1));
+                }
             }
+
+            Pattern pattern1 = Pattern.compile("alt=\"(.*?)\"");
+            Matcher matcher1 = pattern1.matcher(splitResult[0]);
+
+            while (matcher1.find()) {
+                arrayListNames.add(matcher.group(1));
+            }
+
+            Random generateRandomInstances = new Random();
+            randomCelebrity = generateRandomInstances.nextInt(arrayListURLs.size());
 
         } catch (ExecutionException | InterruptedException e) {
 
